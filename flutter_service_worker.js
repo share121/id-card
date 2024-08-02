eecfc91c09758778a3c3d98118b89a36ed3c4ac6
share121 <share121@users.noreply.github.com>
2024-08-02 14:58:51 +0000

    deploy: d8b45358ba8245ed77673bbb32b9ccf45b40354a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6bc58483d4d02f04962f5ae4d31a942e
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "0b040b4ebaa74d644ceb81078cbf4f28",
+"flutter_bootstrap.js": "95386073ad2445e206593a3b20a1a723",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
